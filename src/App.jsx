f313396edc32afc13ba6d668102b27864b891793
Federico Perez <federicoperez@digitalhouse.com>
2024-01-26 02:31:00 -0300

    Updated App structure with styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 
+// Components
+import SliderInpt from './components/SliderInpt/index'
+import Calculation from './components/Calculation/index'
+import ModalBtn from './components/ModalBtn/index'
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  // slider input amount values
+  const minAmount = 5000;
+  const maxAmount = 50000;
+  const [amount, setAmount] = useState(minAmount);
+
+  // slider input duration values
+  const minDuration = 3;
+  const maxDuration = 24;
+  const [duration, setDuration] = useState(minDuration);
+  
+  // total value
+  const [total, setTotal] = useState(0);
+
+  // handlers
+  const handleAmount = (v) => setAmount(v);
+  const handleDuration = (v) => setDuration(v);
+  const handleTotal = (v) => setTotal(v);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="AppContainer">
+      <h2>Simulá tu crédito</h2>
+      <SliderInpt 
+          id="Slider1" 
+          name={"Monto total"} 
+          min={minAmount} 
+          max={maxAmount}
+          symbol={"$"} 
+          handleFn={(v) => handleAmount(v)}/>
+      <SliderInpt 
+          id="Slider2" 
+          name={"Plazo"} 
+          min={minDuration} 
+          max={maxDuration} 
+          handleFn={(v) => handleDuration(v)}/>
+      <Calculation
+        title={"Cuota fija por mes:"} 
+        amount={amount}
+        duration={duration}
+        handleFn={(v) => handleTotal(v)}/>
+      <div id="CtaAction">
+        <ModalBtn 
+          buttonClassName={"credit"}
+          buttonText={"Obtener crédito"}
+          modalTitle={"!Crédito obtenido con éxito¡"}/>
+        <ModalBtn 
+          buttonClassName={"fee"}
+          buttonText={"Ver detalle de cuotas"}
+          modalTitle={"Detalles"}
+          modalMsg={`Tu crédito tiene un valor total de $${amount} y contas con el financiamiento de ${duration} cuotas de $${total}.`}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
